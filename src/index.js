0e651306a95657ab6b4f36e5f4b6b81ed95576df
Krista Goralczyk <krista.goralczyk@gmail.com>
2018-03-13 15:47:43 -0500

    setup & extend airbnb linter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import App from './App';
 import Home from './containers/Home';
 import NotFound from './components/NotFound';
 
-import createHistory from 'history/createBrowserHistory';
-export let history = createHistory();
+const history = createHistory();
+export default history;
 
 ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <Router>
     <App>
-    <Switch>
-        <Route exact path='/' component={Home} />
+      <Switch>
+        <Route exact path="/" component={Home} />
         <Route component={NotFound} />
-    </Switch>
+      </Switch>
     </App>
   </Router>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
